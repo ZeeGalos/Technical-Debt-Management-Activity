c06ead8735abd72b8deb74b404a43a454e28c153
ZaretnoQ <christianseniel2@gmail.com>
2025-03-06 17:12:31 +0800

    Base salary decimal update

--- Base salary deduction code.py
+++ Base salary deduction code.py
@@ -1,8 +1,8 @@
 def compute_deductions(salary):
-    sss_cost = input("Enter SSS deduction value: ")
-    philhealth_cost = (salary * 0.05) / 2
-    pagibig_cost = input("Enter Pagibig deduction value: ")
-    tax_value = input("Enter tax deduction value: ")
+    sss_cost = float(input("Enter SSS deduction value with decimals: "))
+    philhealth_cost = float((salary * 0.05) / 2.0)
+    pagibig_cost = float(input("Enter Pagibig deduction value with decimals: "))
+    tax_value = float(input("Enter tax deduction value with decimals: "))
 
     deductions = sss_cost + philhealth_cost + pagibig_cost + tax_value
     net_salary = salary - deductions
